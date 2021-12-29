05b8fe5f2b785912071854e447e4ce81b4ca4ae4
Manno <si.jorgelucas@gmail.com>
2021-12-29 10:52:30 -0300

    Prova do capítulo
    Uma das questões está com a resposta bugada

---
+++ Cap 3/exercicio6.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Prova 28/32")
+	fmt.Println("https://docs.google.com/forms/d/e/1FAIpQLScmMK7rjqj9SF2qTaN4Vg6mQX19YWqop7WRSfHjxZT-xbqdVQ/viewscore?viewscore=AE0zAgC7CQS6koijHi1lKoWYgb3-ofBVT4Ytycr0-pGCjzlVruv-Ef-6_ucDewsLYQdL-bI")
+}
