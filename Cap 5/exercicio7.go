5fd9916c8929e3b790b56e802dc5d2db3dcc78ee
Manno <si.jorgelucas@gmail.com>
2021-12-31 08:59:44 -0300

    "Prova"

---
+++ Cap 5/exercicio7.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("14/18, fui lerdo")
+	fmt.Println("https://docs.google.com/forms/d/e/1FAIpQLSerPix-DjmElHFdXCz2cGJKqOfJEmnjkMTxxmWNVEVW-Tki5g/viewscore?viewscore=AE0zAgCX7QLQAoTw1PMwsSGHS6Ie6zCk4H3fWXTPM3W6ajqcuCbOEP8icYs-Y66B9PZiL-Q")
+}
